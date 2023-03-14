ca36021a5bc6fac2a78dc3b476cec8f3ffe51f85
wooklogy <wkdgusdnr55@gmail.com>
2023-03-14 22:23:14 +0900

    deploy to gh-pages

---
+++ _next/static/OISOJYtc1KflmcGb9iynl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cff1d6967a549c99.css","static/chunks/pages/index-c7cc90915aae7690.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
